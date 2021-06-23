ab35623637fdc88a5d00395cf88d701e7ec5b4fa
coolgogi <suhyun926@naver.com>
2021-06-23 14:46:34 +0900

    bottomBar & get.tonamed('/upload')

--- herehear/lib/home/home.dart
+++ herehear/lib/home/home.dart
@@ -8,7 +8,9 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("hereNhear")),
-      body: Container(),
+      body: Container(
+        child: Text("Hello world"),
+      ),
     );
   }
 }
