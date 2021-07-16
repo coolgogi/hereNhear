5226fa8b4fa600bc0df47f2cc59b84c0a0a42e09
yulthi <21700550@handong.edu>
2021-07-16 14:40:08 +0900

    yr

--- herehear/lib/subscribed/subscribed.dart
+++ herehear/lib/subscribed/subscribed.dart
@@ -324,11 +324,8 @@ class SubscribedPage extends StatelessWidget {
                       // margin: EdgeInsets.all(0.0.w),
                       width: 70.0.h,
                       height: 70.0.h,
-                      decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(6.0.r) //                 <--- border radius here
-                        ),
+                      child: SizedBox(
+                        child: Image.asset(room['image']),
                       ),
                     ),
                   ),
