d9f6afa58680d643d4b1f40e724cdb54d9a5d898
coolgogi <suhyun926@naver.com>
2021-07-22 04:43:07 +0900

    invitation page 완성

--- herehear/lib/appBar/create_broadcast.dart
+++ herehear/lib/appBar/create_broadcast.dart
@@ -182,7 +182,10 @@ class _CreateBroadcastPageState extends State<CreateBroadcastPage> {
                   width: double.infinity,
                   child: ElevatedButton(
                     onPressed: () {
-                      invitationDialog(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => invite()),
+                      );
                     },
                     child: Text('친구 초대'),
                   ),
@@ -192,95 +195,6 @@ class _CreateBroadcastPageState extends State<CreateBroadcastPage> {
           ],
         ),
       ),
-    );
-  }
-
-  Future<dynamic> invitationDialog(BuildContext context) {
-    List<bool> isChecked = List.filled(5, false);
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("친구 초대"),
-          content: Column(
-            children: [
-              Row(
-                children: [
-                  Text('김한동'),
-                  Checkbox(
-                    value: isChecked[0],
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked[0] = value!;
-                      });
-                    },
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text('박한동'),
-                  Checkbox(
-                    value: isChecked[1],
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked[1] = value!;
-                      });
-                    },
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text('최한동'),
-                  Checkbox(
-                    value: isChecked[2],
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked[2] = value!;
-                      });
-                    },
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text('이한동'),
-                  Checkbox(
-                    value: isChecked[3],
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked[3] = value!;
-                      });
-                    },
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text('한동이'),
-                  Checkbox(
-                    value: isChecked[4],
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked[4] = value!;
-                      });
-                    },
-                  )
-                ],
-              )
-            ],
-          ),
-          actions: <Widget>[
-            new TextButton(
-              child: Text("보내기"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 
