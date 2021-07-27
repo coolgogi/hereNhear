60f6f2a45144142e2cc19cfcae900b8ef6983f73
CaptainJang316 <gksrkfkaryghl@gmail.com>
2021-07-27 18:22:15 +0900

    asdf

--- herehear/lib/main.dart
+++ herehear/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:herehear/appBar/notification.dart';
+import 'package:herehear/appBar/notification/notification.dart';
 import 'package:herehear/location_data/location.dart';
 import 'package:herehear/bottomNavigationBar/search/search.dart';
 import 'package:herehear/bottomNavigationBar/home/HomePage.dart';
