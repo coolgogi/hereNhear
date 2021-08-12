58802f7d22af3e9a0ad7b3bc9a1c28e8c2e38945
coolgogi <suhyun926@naver.com>
2021-08-12 15:41:58 +0900

    tts complete in android but I don'\''t know in iOS. I can'\''t test.

--- herehear/lib/login/agreeToS.dart
+++ herehear/lib/login/agreeToS.dart
@@ -7,7 +7,6 @@ import 'package:herehear/login/signUp_complete.dart';
 import 'signUp_controller.dart';
 
 class AgreementTOSPage extends StatelessWidget {
-
   final registerController = Get.put(RegisterController());
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -22,105 +21,148 @@ class AgreementTOSPage extends StatelessWidget {
             Padding(
               padding: EdgeInsets.fromLTRB(40.w, 100.h, 42.w, 0.h),
               child: Obx(() => Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('이용약관에 동의하시면', style: Theme.of(context).textTheme.caption!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('이용약관에 동의하시면',
+                          style: Theme.of(context).textTheme.caption!.copyWith(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                              )),
+                      Text('회원가입이 완료됩니다.',
+                          style: Theme.of(context).textTheme.caption!.copyWith(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                              )),
+                      SizedBox(height: 130.h),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 6.w),
+                            child: Checkbox(
+                                value: registerController
+                                    .allAgree.value, //처음엔 false
+                                onChanged: (value) {
+                                  registerController.allAgree.value = value!;
+                                  registerController.firstTermAgree.value =
+                                      value;
+                                  registerController.secondTermAgree.value =
+                                      value;
+                                  registerController.thirdTermAgree.value =
+                                      value;
+                                }),
+                          ),
+                          Text(
+                            '전체 약관에 동의합니다.',
+                            style: Theme.of(context).textTheme.subtitle1,
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 6.h),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 6.w),
+                            child: Checkbox(
+                              value: registerController
+                                  .firstTermAgree.value, //처음엔 false
+                              onChanged: (value) => registerController
+                                  .firstTermAgree.value = value!,
+                            ),
+                          ),
+                          Text(
+                            '서비스 이용약관',
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                          Text('(필수)',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .primary)),
+                          Expanded(child: Container()),
+                          IconButton(
+                              onPressed: null, icon: Icon(Icons.expand_more))
+                        ],
+                      ),
+                      SizedBox(height: 6.h),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 6.w),
+                            child: Checkbox(
+                              value: registerController
+                                  .secondTermAgree.value, //처음엔 false
+                              onChanged: (value) => registerController
+                                  .secondTermAgree.value = value!,
+                            ),
+                          ),
+                          Text(
+                            '개인정보처리방침',
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                          Text('(필수)',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .primary)),
+                          Expanded(child: Container()),
+                          IconButton(
+                              onPressed: null, icon: Icon(Icons.expand_more))
+                        ],
+                      ),
+                      SizedBox(height: 6.h),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 6.w),
+                            child: Checkbox(
+                              value: registerController
+                                  .thirdTermAgree.value, //처음엔 false
+                              onChanged: (value) => registerController
+                                  .thirdTermAgree.value = value!,
+                            ),
+                          ),
+                          Text(
+                            '서비스 이용약관(선택)',
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                          Expanded(child: Container()),
+                          IconButton(
+                              onPressed: null, icon: Icon(Icons.expand_more))
+                        ],
+                      ),
+                    ],
                   )),
-                  Text('회원가입이 완료됩니다.', style: Theme.of(context).textTheme.caption!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  )),
-                  SizedBox(height: 130.h),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 6.w),
-                        child: Checkbox(
-                            value: registerController.allAgree.value, //처음엔 false
-                            onChanged: (value) {
-                              registerController.allAgree.value = value!;
-                              registerController.firstTermAgree.value = value!;
-                              registerController.secondTermAgree.value = value!;
-                              registerController.thirdTermAgree.value = value!;
-                            }
-                        ),
-                      ),
-                      Text('전체 약관에 동의합니다.', style: Theme.of(context).textTheme.subtitle1,)
-                    ],
-                  ),
-                  SizedBox(height: 6.h),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 6.w),
-                        child: Checkbox(
-                          value: registerController.firstTermAgree.value, //처음엔 false
-                          onChanged: (value) => registerController.firstTermAgree.value = value!,
-                        ),
-                      ),
-                      Text('서비스 이용약관', style: Theme.of(context).textTheme.subtitle1,),
-                      Text('(필수)', style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                          color: Theme.of(context).colorScheme.primary
-                      )),
-                      Expanded(child: Container()),
-                      IconButton(onPressed: null, icon: Icon(Icons.expand_more))
-                    ],
-                  ),
-                  SizedBox(height: 6.h),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 6.w),
-                        child: Checkbox(
-                          value: registerController.secondTermAgree.value, //처음엔 false
-                          onChanged: (value) => registerController.secondTermAgree.value = value!,
-                        ),
-                      ),
-                      Text('개인정보처리방침', style: Theme.of(context).textTheme.subtitle1,),
-                      Text('(필수)', style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                          color: Theme.of(context).colorScheme.primary
-                      )),
-                      Expanded(child: Container()),
-                      IconButton(onPressed: null, icon: Icon(Icons.expand_more))
-                    ],
-                  ),
-                  SizedBox(height: 6.h),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 6.w),
-                        child: Checkbox(
-                          value: registerController.thirdTermAgree.value, //처음엔 false
-                          onChanged: (value) => registerController.thirdTermAgree.value = value!,
-                        ),
-                      ),
-                      Text('서비스 이용약관(선택)', style: Theme.of(context).textTheme.subtitle1,),
-                      Expanded(child: Container()),
-                      IconButton(onPressed: null, icon: Icon(Icons.expand_more))
-                    ],
-                  ),
-                ],
-              )),
             ),
             Expanded(child: Container()),
             Obx(() => SizedBox(
-              height: 44.h,
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  elevation: MaterialStateProperty.all(0),
-                  backgroundColor: (registerController.firstTermAgree.value && registerController.secondTermAgree.value)?
-                  MaterialStateProperty.all(Theme.of(context).colorScheme.primary) : MaterialStateProperty.all(Theme.of(context).colorScheme.onSurface),
-                ),
-                onPressed: () {
-                  if(_formKey.currentState!.validate())
-                    Get.to(RegisterCompletePage());
-                },
-                child: Text('다음', style: Theme.of(context).textTheme.headline4!.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary
+                  height: 44.h,
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all(0),
+                      backgroundColor:
+                          (registerController.firstTermAgree.value &&
+                                  registerController.secondTermAgree.value)
+                              ? MaterialStateProperty.all(
+                                  Theme.of(context).colorScheme.primary)
+                              : MaterialStateProperty.all(
+                                  Theme.of(context).colorScheme.onSurface),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate())
+                        Get.to(RegisterCompletePage());
+                    },
+                    child: Text('다음',
+                        style: Theme.of(context).textTheme.headline4!.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary)),
+                  ),
                 )),
-              ),
-            )),
           ],
         ),
       ),
