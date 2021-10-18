311b11dc3d19c726b6bc59b38e6a969ed2cdbefb
yulthi <21700550@handong.edu>
2021-10-18 13:30:00 +0900

    yr

--- herehear/lib/broadcast/timer_controller.dart
+++ herehear/lib/broadcast/timer_controller.dart
@@ -6,22 +6,10 @@ import 'package:get/get.dart';
 
 class TimerContorller extends GetxController {
   var time = 0.obs;
-  RxString sec = ''.obs;
-  RxString min = ''.obs;
-  RxString hour = ''.obs;
 
   callIncrement() {
 
     time.value++;
-    sec =RxString((time.value % 60).floor().toString().padLeft(2, '0'));
-    min =RxString (((time.value / 60) % 60)
-        .floor()
-        .toString()
-        .padLeft(2, '0'));
-    hour = RxString(((time.value / (60 * 60)) % 60)
-        .floor()
-        .toString()
-        .padLeft(2, '0'));
 
 
   }
